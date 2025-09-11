77a560ace19d23bdb2058ec520fc539dd3196156
olafurj <olafurj@tern.is>
2025-09-11 21:02:38 +0000

    indx

---
+++ index.js
@@ -0,0 +1,13 @@
+export default {
+  async fetch(req) {
+    const modelUrl = "https://github.com/olafurjohannsson/models/raw/2f34e31179390e531f44f0850b24c1f9614e4422/ggml-tiny.en.bin";
+    const resp = await fetch(modelUrl);
+
+    return new Response(resp.body, {
+      headers: {
+        "Content-Type": "application/octet-stream",
+        "Cross-Origin-Resource-Policy": "same-origin",
+      },
+    });
+  },
+};
