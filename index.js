fc3e4be10a2025cd5f3ffec0022bf9ed5f4261c0
olafurj <olafurj@tern.is>
2025-09-11 21:24:32 +0000

    models

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(req) {
-    const modelUrl = "https://github.com/olafurjohannsson/models/raw/2f34e31179390e531f44f0850b24c1f9614e4422/ggml-tiny.en.bin";
+    const modelUrl = "https://raw.githubusercontent.com/olafurjohannsson/models/main/ggml-tiny.en.bin";
     const resp = await fetch(modelUrl);
 
     return new Response(resp.body, {
